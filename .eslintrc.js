ac7c4aa46028baaee844d8f6346b5c3eee4a62a7
Zachary Hueras <zhueras@amdtelemedicine.com>
2021-07-06 04:01:15 -0400

    Move .eslintrc to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    env: {
+        browser: true,
+        node: true,
+        es2021: true,
+    },
+    rules: {
+        indent: ["error", 4]
+    }
+}
