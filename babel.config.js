7555011a9cc298e1c7640181da04eba82032fef2
Zachary Hueras <zhueras@amdtelemedicine.com>
2021-08-31 02:32:38 -0400

    Disable sourcemaps

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function(api) {
     api.cache(true);
     return {
-        sourceMaps: 'both',
+        sourceMaps: false,
         presets: [
             [
                 '@babel/preset-env',
