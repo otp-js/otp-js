ab3cd72ac7b172950527a1bc2cd59beae87518f8
Zachary Hueras <zhueras@amdtelemedicine.com>
2021-11-13 16:03:08 -0500

    supervisor: reexport existing permanent, temporary

--- packages/supervisor/src/symbols.js
+++ packages/supervisor/src/symbols.js
@@ -1,3 +1,5 @@
+import { Symbols } from '@otpjs/core';
+
 export const simple_one_for_one = Symbol.for('simple_one_for_one');
 export const one_for_one = Symbol.for('one_for_one');
 export const rest_for_one = Symbol.for('rest_for_one');
@@ -6,6 +8,5 @@ export const one_for_all = Symbol.for('one_for_all');
 export const which_children = Symbol.for('which_children');
 export const count_children = Symbol.for('count_children');
 
-export const temporary = Symbol.for('temporary');
 export const transient = Symbol.for('transient');
-export const permanent = Symbol.for('permanent');
+export const { temporary, permanent } = Symbols;
