ebdadec475e55154fa63144e2e0e065b0b95fa99
Zachary Hueras <zhueras@amdtelemedicine.com>
2021-11-27 02:09:20 -0500

    root: include .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+    useTabs: false,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'avoid',
+};
