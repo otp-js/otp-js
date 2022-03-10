84aceb667403172d612503356fa28e01c40df78b
Zachary Hueras <zhueras@amdtelemedicine.com>
2022-03-09 20:30:59 -0500

    packages/gen: fix cast symbols

--- packages/gen/src/symbols.js
+++ packages/gen/src/symbols.js
@@ -1,5 +1,7 @@
-export const call = Symbol.for('call');
-export const cast = Symbol.for('cast');
+export const call = Symbol.for('$gen_call');
+export const cast = Symbol.for('$gen_cast');
+export const $gen_call = Symbol.for('$gen_call');
+export const $gen_cast = Symbol.for('$gen_cast');
 export const monitor = Symbol.for('monitor');
 export const link = Symbol.for('link');
 export const nolink = Symbol.for('nolink');
