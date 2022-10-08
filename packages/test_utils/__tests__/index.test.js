23fc93661c9ecd4b6c174ead54d5ea4ed0736d35
Zachary Hueras <zhueras@amdtelemedicine.com>
2022-10-07 21:56:54 -0400

    feat(test_utils): pattern matchers for mock calls

--- packages/test_utils/__tests__/index.test.js
+++ packages/test_utils/__tests__/index.test.js
@@ -1,6 +1,131 @@
 import '../src';
 
 describe('@otpjs/test_utils', function () {
+    describe('toHaveBeenNthCalledWithPattern', function () {
+        it('matches against the arguments of a specific call', function () {
+            const fn = jest.fn(() => true);
+
+            fn('a', 1, NaN);
+            fn(NaN, 'b', 2);
+            fn([1, 2, 3], 3, 'c');
+
+            expect(function () {
+                expect(fn).toHaveBeenNthCalledWithPattern(
+                    0,
+                    'a',
+                    Number.isInteger,
+                    Number.isNaN
+                );
+            }).not.toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenNthCalledWithPattern(
+                    0,
+                    Array.isArray,
+                    Number.isInteger,
+                    'c'
+                );
+            }).toThrow();
+
+            expect(function () {
+                expect(fn).toHaveBeenNthCalledWithPattern(
+                    1,
+                    Number.isNaN,
+                    'b',
+                    Number.isInteger
+                );
+            }).not.toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenNthCalledWithPattern(1, 'a', Number.isNaN);
+            }).toThrow();
+
+            expect(function () {
+                expect(fn).toHaveBeenNthCalledWithPattern(
+                    2,
+                    Number.isNaN,
+                    'b',
+                    Number.isInteger
+                );
+            }).toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenNthCalledWithPattern(
+                    2,
+                    Array.isArray,
+                    Number.isInteger,
+                    'c'
+                );
+            }).not.toThrow();
+        });
+    });
+    describe('toHaveBeenLastCalledWithPattern', function () {
+        it('matches against the arguments of the latest call', function () {
+            const fn = jest.fn(() => true);
+
+            fn(1, 2, 3);
+            expect(function () {
+                expect(fn).toHaveBeenLastCalledWithPattern(
+                    Number.isInteger,
+                    Number.isInteger,
+                    Number.isInteger
+                );
+            }).not.toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenLastCalledWithPattern(1, 2, 3);
+            }).not.toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenLastCalledWithPattern('a', 'b', 'c');
+            }).toThrow();
+
+            fn('a', 'b', 'c');
+            expect(function () {
+                expect(fn).toHaveBeenLastCalledWithPattern(
+                    Number.isInteger,
+                    Number.isInteger,
+                    Number.isInteger
+                );
+            }).toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenLastCalledWithPattern(1, 2, 3);
+            }).toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenLastCalledWithPattern('a', 'b', 'c');
+            }).not.toThrow();
+        });
+    });
+    describe('toHaveBeenCalledWithPattern', function () {
+        it('matches against the arguments of the any call', function () {
+            const fn = jest.fn(() => true);
+
+            fn(1, 2, 3);
+            expect(function () {
+                expect(fn).toHaveBeenCalledWithPattern(
+                    Number.isInteger,
+                    Number.isInteger,
+                    Number.isInteger
+                );
+            }).not.toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenCalledWithPattern(1, 2, 3);
+            }).not.toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenCalledWithPattern('a', 'b', 'c');
+            }).toThrow();
+
+            fn('a', 'b', 'c');
+            expect(function () {
+                expect(fn).toHaveBeenCalledWithPattern(
+                    Number.isInteger,
+                    Number.isInteger,
+                    Number.isInteger
+                );
+            }).not.toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenCalledWithPattern(1, 2, 3);
+            }).not.toThrow();
+            expect(function () {
+                expect(fn).toHaveBeenCalledWithPattern('a', 'b', 'c');
+            }).not.toThrow();
+        });
+    });
     describe('toMatchPattern', function () {
         it('exists', function () {
             expect(expect(1).toMatchPattern).toBeInstanceOf(Function);
--- packages/test_utils/src/index.js
+++ packages/test_utils/src/index.js
@@ -7,6 +7,90 @@ const log = debug('otpjs:test_utils');
 Error.stackTraceLimit = Infinity;
 
 expect.extend({
+    toHaveBeenNthCalledWithPattern(received, callIndex, ...args) {
+        const compiled = matching.compile(args);
+        const pass = compiled(received.mock.calls[callIndex]);
+
+        if (pass) {
+            return {
+                pass: true,
+                message: () =>
+                    `expected ${util
+                        .inspect(received.mock.calls[callIndex])
+                        .replace(
+                            /[\r\n]+/g,
+                            ' '
+                        )} not to match the pattern ${util
+                        .inspect(args)
+                        .replace(/[\r\n]+/g, ' ')}`,
+            };
+        } else {
+            return {
+                pass: false,
+                message: () =>
+                    `expected ${util
+                        .inspect(received.mock.calls[callIndex])
+                        .replace(/[\r\n]+/g, ' ')} to match the pattern ${util
+                        .inspect(args)
+                        .replace(/[\r\n]+/g, ' ')}`,
+            };
+        }
+    },
+    toHaveBeenLastCalledWithPattern(received, ...args) {
+        const compiled = matching.compile(args);
+        const pass = compiled(received.mock.lastCall);
+
+        if (pass) {
+            return {
+                pass: true,
+                message: () =>
+                    `expected ${util
+                        .inspect(received.mock.lastCall)
+                        .replace(
+                            /[\r\n]+/g,
+                            ' '
+                        )} not to match the pattern ${util
+                        .inspect(args)
+                        .replace(/[\r\n]+/g, ' ')}`,
+            };
+        } else {
+            return {
+                pass: false,
+                message: () =>
+                    `expected ${util
+                        .inspect(received.mock.lastCall)
+                        .replace(/[\r\n]+/g, ' ')} to match the pattern ${util
+                        .inspect(args)
+                        .replace(/[\r\n]+/g, ' ')}`,
+            };
+        }
+    },
+    toHaveBeenCalledWithPattern(received, ...args) {
+        const compiled = matching.compile(args);
+        let pass = false;
+        let callIndex = 0;
+        do {
+            pass = compiled(received.mock.calls[callIndex++]);
+        } while (!pass && callIndex < received.mock.calls.length);
+
+        if (pass) {
+            return {
+                pass: true,
+                message: () =>
+                    `expected function not to have been called with arguments matching the pattern ${util
+                        .inspect(args)
+                        .replace(/[\r\n]+/g, ' ')}`,
+            };
+        } else {
+            return {
+                pass: false,
+                message: () =>
+                    `expected function to have been called with arguments matching the pattern ${util
+                        .inspect(args)
+                        .replace(/[\r\n]+/g, ' ')}`,
+            };
+        }
+    },
     toMatchPattern(received, pattern) {
         log('toMatchPattern(%o, %o)', received, pattern);
         const compiled = matching.compile(pattern);
