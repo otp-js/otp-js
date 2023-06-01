5808a8675e9db324a2945aad819ca37c40477fda
Zachary Hueras <zhueras@amdtelemedicine.com>
2023-06-01 18:21:17 -0400

    fix(types): smarter OTPError constructor

--- packages/types/src/error.js
+++ packages/types/src/error.js
@@ -1,24 +1,35 @@
-export function OTPError(term, capture) {
-    if (!capture) {
-        capture = Object.create(OTPError.prototype);
-        Error.captureStackTrace(capture, OTPError);
+import debug from 'debug';
+
+const log = debug('otp:types:error');
+
+export function OTPError(term) {
+    let constructed;
+    log('OTPError(term: %o, new.target: %o)', term, new.target);
+
+    if (new.target === undefined) {
+        constructed = Object.create(OTPError.prototype);
+    } else {
+        constructed = this;
     }
 
-    Object.defineProperty(capture, 'term', {
+    Error.captureStackTrace(constructed, OTPError);
+
+    Object.defineProperty(constructed, 'term', {
         get() {
             return term;
         },
         configurable: false,
         enumerable: true,
     });
-    Object.defineProperty(capture, 'message', {
+    Object.defineProperty(constructed, 'message', {
         get() {
             return String(term);
         },
         configurable: false,
         enumerable: true,
     });
-    return capture;
+
+    return constructed;
 }
 
 OTPError.prototype = Object.create(Error.prototype);
