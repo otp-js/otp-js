ceb7d00887470bb4cb50b2c7585f53b0d05532cb
Zachary Hueras <zhueras@amdtelemedicine.com>
2023-06-07 21:27:37 -0400

    chore: add jest.config.js files

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -5,17 +5,8 @@ module.exports = {
         '\\.jsx?$': ['babel-jest', { rootMode: 'upward' }],
     },
     moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*.js'],
     testEnvironment: 'node',
     testPathIgnorePatterns: ['lib/'],
-    coveragePathIgnorePatterns: ['lib/', '__tests__/'],
-    coverageReporters: [
-        'json',
-        'text',
-        'clover',
-        ['lcov', { projectRoot: __dirname }],
-    ],
     projects: [
         {
             displayName: 'node',
