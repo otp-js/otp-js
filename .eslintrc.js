84cebd66b9be72eac7dae2810906a156c5e5bcb4
Zachary Hueras <zhueras@amdtelemedicine.com>
2023-06-16 20:28:44 -0400

    chore: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,53 @@
 module.exports = {
-    extends: 'eslint:recommended',
     env: {
         browser: true,
-        node: true,
         es2021: true,
+        node: true
+    },
+    extends: ['eslint:recommended', 'semistandard'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
     },
     rules: {
-        indent: ["error", 4]
+        indent: [
+            'error',
+            4,
+            { SwitchCase: 1, MemberExpression: 1 }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        quotes: [
+            'error',
+            'single'
+        ],
+        semi: [
+            'error',
+            'always'
+        ],
+        'max-len': [
+            'error',
+            {
+                code: 120,
+                ignoreTrailingComments: true,
+                ignoreTemplateLiterals: true,
+                ignoreStrings: true
+            }
+        ],
+        'space-before-function-paren': ['off'],
+        camelcase: [
+            'off'
+        ],
+        'no-unused-vars': [
+            'error',
+            {
+                varsIgnorePattern: '^_',
+                argsIgnorePattern: '^_',
+                caughtErrorsIgnorePattern: '^_',
+                destructuredArrayIgnorePattern: '^_'
+            }
+        ]
     }
-}
+};
