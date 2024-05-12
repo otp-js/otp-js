8f45bbc2e2b33d64ae146de5d8be72d0191510ec
Zachary Hueras <zachary.hueras@gmail.com>
2024-05-12 02:28:58 -0400

    chore: update jest config, gitignore

--- jest.config.js
+++ jest.config.js
@@ -29,6 +29,8 @@ module.exports = {
         'packages/gen/jest.config.js',
         'packages/gen_server/jest.config.js',
         'packages/matching/jest.config.js',
+        'packages/net/jest.config.js',
+        'packages/node/jest.config.js',
         'packages/proc_lib/jest.config.js',
         'packages/supervisor/jest.config.js',
         'packages/test_utils/jest.config.js',
